c28b5a3e98f98e28d8c4ccfd533b56f59d462fee
Shubham Chandak <schandak@stanford.edu>
2019-06-09 19:39:01 -0700

    update logs

--- basic_tests.py
+++ basic_tests.py
@@ -28,9 +28,6 @@ def test_general(code: stabilizer_code.StabilizerCode, initial_state_prep: Progr
         inverse_initial_state_prep = inverse_initial_state_prep_param
     prog = initial_state_prep + code.encoding_program + error_prog + code.decoding_program + inverse_initial_state_prep
 
-
-
-
     prog.measure_all()
     measured_bits = np.array(qvm.run(prog, trials=num_trials))
     decoded_msg_bits = measured_bits[:,:code.k]
@@ -44,23 +41,26 @@ def generate_initial_state_prep_for_testing(k):
     # returns list of programs generating initial states for testing the
     # encoding decoding.
     if k == 1:
-        # for k = 1, we return |0>, |1>, |+>, |->, and a few more
+        # for k = 1, we return |0>, |1>, |+>, and a few random Bloch sphere points
         return [
             Program(),
             Program('X 0'),
-        ]+[Program('RX ('+str(np.random.rand(1)[0]*np.pi)+') 0') + Program('RZ ('+str(np.random.rand(1)[0]*2*np.pi)+') 0')]*5
+            Program('H 0'),
+        ]+[Program('RX ('+str(np.random.rand(1)[0]*np.pi)+') 0') + Program('RZ ('+str(np.random.rand(1)[0]*2*np.pi)+') 0')]*3
     else:
         raise NotImplementedError
 
-def test_no_error(code: stabilizer_code.StabilizerCode):
+def test_no_error(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('No error case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
         print('Initial state')
         print(init_program)
-        test_general(code, init_program, Program())
+        num_errors = test_general(code, init_program, Program(), num_trials)
+        print('num_trials', num_trials)
+        print('num_errors', num_errors)
 
-def test_single_bit_flip(code: stabilizer_code.StabilizerCode):
+def test_single_bit_flip(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('Single bit flip case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
@@ -68,10 +68,12 @@ def test_single_bit_flip(code: stabilizer_code.StabilizerCode):
         print(init_program)
         for i in range(code.n):
             print('Error in qubit',i)
-            test_general(code, init_program, Program('X '+str(i)))
+            num_errors = test_general(code, init_program, Program('X '+str(i)), num_trials)
+            print('num_trials', num_trials)
+            print('num_errors', num_errors)
 
 
-def test_single_phase_flip(code: stabilizer_code.StabilizerCode):
+def test_single_phase_flip(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('Single phase flip case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
@@ -79,10 +81,11 @@ def test_single_phase_flip(code: stabilizer_code.StabilizerCode):
         print(init_program)
         for i in range(code.n):
             print('Error in qubit',i)
-            test_general(code, init_program, Program('Z '+str(i)))
+            num_errors = test_general(code, init_program, Program('Z '+str(i)), num_trials)
+            print('num_trials', num_trials)
+            print('num_errors', num_errors)
 
-
-def test_single_Y_noise(code: stabilizer_code.StabilizerCode):
+def test_single_Y_noise(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('Single Y error case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
@@ -90,9 +93,11 @@ def test_single_Y_noise(code: stabilizer_code.StabilizerCode):
         print(init_program)
         for i in range(code.n):
             print('Error in qubit',i)
-            test_general(code, init_program, Program('Y '+str(i)))
+            num_errors = test_general(code, init_program, Program('Y '+str(i)), num_trials)
+            print('num_trials', num_trials)
+            print('num_errors', num_errors)
 
-def test_single_H_error(code: stabilizer_code.StabilizerCode):
+def test_single_H_error(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('Single Hadamard error case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
@@ -100,9 +105,11 @@ def test_single_H_error(code: stabilizer_code.StabilizerCode):
         print(init_program)
         for i in range(code.n):
             print('Error in qubit',i)
-            test_general(code, init_program, Program('H '+str(i)))
+            num_errors = test_general(code, init_program, Program('H '+str(i)), num_trials)
+            print('num_trials', num_trials)
+            print('num_errors', num_errors)
 
-def test_oneX_oneZ(code: stabilizer_code.StabilizerCode):
+def test_oneX_oneZ(code: stabilizer_code.StabilizerCode, num_trials = 100):
     print('One X and one Z error on different qubits case')
     initial_state_list = generate_initial_state_prep_for_testing(code.k)
     for init_program in initial_state_list:
@@ -114,8 +121,9 @@ def test_oneX_oneZ(code: stabilizer_code.StabilizerCode):
                     continue
                 print('X error in qubit',i)
                 print('Z error in qubit',j)
-                test_general(code, init_program, Program('X '+str(i))+Program('Z '+str(j)))
-
+                num_errors = test_general(code, init_program, Program('X '+str(i))+Program('Z '+str(j)), num_trials)
+                print('num_trials', num_trials)
+                print('num_errors', num_errors)
 def main():
 	if len(sys.argv) == 1:
 		# test all codes
